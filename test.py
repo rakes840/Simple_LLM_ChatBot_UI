f44b763ce988550b3eed52233ae547578a028605
rakes840 <64484756+rakes840@users.noreply.github.com>
2025-06-16 16:54:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-I'm sorry, I encountered an error while processing your request. Please try again later. Error: 401 Client Error: Unauthorized for url: https://huggingface.co/api/models/mistralai/Mistral-7B-Instruct-v0.3?expand=inferenceProviderMapping (Request ID: Root=1-6831fae1-5123586040931d986fd03209;58257bd7-c400-4b25-9303-58bc430e02a1)
+from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper,ArxivAPIWrapper
+api_wrapper_arxiv=ArxivAPIWrapper(top_k_results=2,doc_content_chars_max=500)
+arxiv=ArxivQueryRun(api_wrapper=api_wrapper_arxiv)
+print(arxiv.name)
+arxiv.invoke("Attention is all you need")
 
-Invalid credentials in Authorization header
+
+SSLError: HTTPSConnectionPool(host='export.arxiv.org', port=443): Max retries exceeded with url: /api/query?search_query=Attention+is+
+all+you+need&id_list=&sortBy=relevance&sortOrder=descending&start=0&max_results=100 
+(Caused by SSLError(SSLCertVerificationError
+                    (1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1006)')))
