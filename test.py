4cf5f6a72521574dba4a0d6c6e3f6d3545bf005a
rakes840 <64484756+rakes840@users.noreply.github.com>
2025-08-03 21:37:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
-from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun
-from langchain_community.utilities import WikipediaAPIWrapper,ArxivAPIWrapper
-api_wrapper_arxiv=ArxivAPIWrapper(top_k_results=2,doc_content_chars_max=500)
-arxiv=ArxivQueryRun(api_wrapper=api_wrapper_arxiv)
-print(arxiv.name)
-arxiv.invoke("Attention is all you need")
-
-
-SSLError: HTTPSConnectionPool(host='export.arxiv.org', port=443): Max retries exceeded with url: /api/query?search_query=Attention+is+
-all+you+need&id_list=&sortBy=relevance&sortOrder=descending&start=0&max_results=100 
-(Caused by SSLError(SSLCertVerificationError
-                    (1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1006)')))
+File "C:\Rakesh\Legal_Case\legal-case-management-main\main.py", line 49, in <module>
+    from rag_pipline import DocumentIngestor
+  File "C:\Rakesh\Legal_Case\legal-case-management-main\rag_pipline.py", line 27, in <module>
+    import fitz  # PyMuPDF for PDF processing
+    ^^^^^^^^^^^
+  File "c:\Rakesh\Legal_Case\legal-case-management-main\.venv\Lib\site-packages\fitz\__init__.py", line 1, in <module>  
+    from frontend import *
+ModuleNotFoundError: No module named 'frontend'
